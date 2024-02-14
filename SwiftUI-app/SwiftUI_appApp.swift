e718aefe434046f60876e820c59d893cffe49e77
KikiSpace <xzhangkiki@gmail.com>
2024-02-14 15:23:03 -0800

    Initial Commit

---
+++ SwiftUI-app/SwiftUI_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_appApp.swift
+//  SwiftUI-app
+//
+//  Created by Xinqi Zhang on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
