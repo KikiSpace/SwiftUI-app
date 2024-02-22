9dbf5ecb3194139f1a78eb54bd67793d81d76146
KikiSpace <xzhangkiki@gmail.com>
2024-02-22 15:00:57 -0800

    Basic finish

---
+++ SwiftUI-app/Views/BadgeBackground.swift
@@ -0,0 +1,18 @@
+//
+//  BadgeBackground.swift
+//  SwiftUI-app
+//
+//  Created by Xinqi Zhang on 2/19/24.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Text("Hello!")
+    }
+}
+
+#Preview {
+    BadgeBackground()
+}
